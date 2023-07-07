c48afb3aafcc9cfafe09434f81144b74906df078
Varun A P <varunlakshmananap@gmail.com>
2023-07-08 01:14:37 +0200

    fix firebase config with next_public

--- src/lib/auth/config.ts
+++ src/lib/auth/config.ts
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
-  authDomain: process.env.FIREBASE_CLIENT_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_CLIENT_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_CLIENT_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_CLIENT_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_CLIENT_APP_ID,
-  measurementId: process.env.FIREBASE_CLIENT_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_APP_ID,
 };
 
 // Initialize Firebase
-let firebase_app =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+let firebase_app = initializeApp(firebaseConfig);
 
 export default firebase_app;
