309ca14c5e68da43e40e3ffa9952c33b1a748f78
Swayam Mohanty <smohanty@travix.com>
2023-07-08 03:25:58 +0200

    Button styles added

--- src/pages/dj/signup/index.tsx
+++ src/pages/dj/signup/index.tsx
@@ -7,9 +7,18 @@ const handleClick = () => {
 };
 
 const SignUp1 = () => {
-  return <div>
-    <Button type="button" value="Click Me" onClick={handleClick} />
-  </div>;
+  return <main>
+    <h1 className="text-white font-sf-pro text-4xl font-bold leading-13">
+      Its how DJ’s make side income
+    </h1>
+    <h3 className="text-white font-sf-pro text-lg font-medium leading-7">
+      Let your audience request tracks and tip you during your live sets.
+    </h3>
+    <Button type="button" value="Click Me" onClick={handleClick} color="primary" />
+    <br></br>
+    <Button type="button" value="Continue with Google" onClick={handleClick} color="primary"/>
+
+  </main>;
 };
 
 export default SignUp1;
