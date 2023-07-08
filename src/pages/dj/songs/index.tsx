b4e7624c5d0626e6b538fcf7913c1cc58c1e2838
Swayam Mohanty <smohanty@travix.com>
2023-07-08 07:22:59 +0200

    Added songs.json for fetching playlists

--- src/pages/dj/songs/index.tsx
+++ src/pages/dj/songs/index.tsx
@@ -1,5 +1,6 @@
 
 import songs from "@/data/songs.json";
+import { Avatar } from "@/lib/components/Avatar";
 import { useEffect, useState } from "react";
 
 
@@ -8,14 +9,44 @@ const MySongList = () => {
     const [data, setData] = useState(songs);
 
     return (
-        <ul>
+        <main className="px-5 py-2 min-h-screen flex flex-col justify-between">
+            <div className="flex flex-row justify-center flex-1 items-center">
+                <Avatar image="/images/profile_pic.png" onChange={() => { }} />
+            </div>
+            <div className="flex flex-row justify-center flex-1 items-center">
+                <p className="text-lg text-bold">
+                    DJ Nifty <span className="text-base">( NL )</span>
+                </p>
+            </div>
+            <div className="flex flex-row justify-center flex-1 items-center py-10 border-b border-white">
+                <h2 className="text-xl text-bold border-r border-white pr-5 py-4" >
+                    {/* {(Math.random() * 10).toFixed(2)} */}
+                    5,00
+                </h2>
+
+                <h2 className="text-xl text-bold border-r border-white pr-5 pl-5 py-4">
+                    {/* {(Math.random() * 10).toFixed(2)} */}
+                    10,00
+                </h2>
+                <h2 className="text-xl text-bold pl-5 py-4">
+                    {/* {(Math.random() * 10).toFixed(2)} */}
+                    0,00
+                </h2>
+            </div>
+
             {data.songs.map((item: any, index) => (
-                <>
-                    <h4 key={index}> {item.title}</h4>
-                    <p>{item.artist}</p>
-                </>
+                <div className=" flex py-6 border-b border-white justify-between" key={index}>
+                    <div className="h-20 w-20">
+                        <Avatar image="/images/profile_pic.png" />
+                    </div>
+                    <div className="px-2 py-1">
+                        <h4 > {item.title}</h4>
+                        <p >{item.artist}</p>
+                    </div>
+
+                </div>
             ))}
-        </ul>
+        </main>
     );
 }
 export default MySongList;
