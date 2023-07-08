8fda90f84e9955a6995a808383b5ec013dfb04da
Swayam Mohanty <smohanty@travix.com>
2023-07-08 12:27:19 +0200

    Added Confirmation page changes

--- src/pages/user/qr/request.tsx
+++ src/pages/user/qr/request.tsx
@@ -1,24 +1,67 @@
-import { AuthLayoutWrapper } from "@/lib/auth/AuthLayout";
 import { useRouter } from "next/router";
-import { useCallback, useEffect, useMemo } from "react";
-import { songs } from "@/data/songs.json";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import { LoadingSpinner } from "@/lib/components/LoadingSpinner";
-import { Input } from "@/lib/components/Input";
 import { User } from "@/lib/modals/user";
-import { useForm } from "react-hook-form";
-import { RequestData, addRequest, updateRequest } from "@/lib/modals/requests";
-import Button from "@/lib/components/Button";
+
+import { Avatar } from "@/lib/components/Avatar";
+import { getUser } from "@/lib/modals/user";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
 
 const RequestPage = () => {
   const {
-    query: { request_id },
+    query: { dj_id },
   } = useRouter();
 
+  const [dj, setDj] = useState<User>();
+
+  useEffect(() => {
+    (async () => {
+      if (!dj_id || typeof dj_id !== "string") return;
+      const { result, error } = await getUser(dj_id);
+      setDj(result || undefined);
+    })();
+  }, [dj_id]);
+
+  if (!dj)
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <LoadingSpinner />
+      </div>
+    );
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p>request_id: {request_id}</p>
-    </div>
+    <motion.main initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="px-5 py-2 min-h-screen flex flex-col justify-between">
+      <div className="flex flex-row justify-center flex-1 items-center mt-16 mb-5">
+        <Avatar image={dj?.photoUrl} onChange={() => { }} />
+      </div>
+      <div className="flex flex-row justify-start flex-1 items-center">
+        <h1 className="text-4xl text-bold">
+          Thanks for your
+          <h1 className="text-6xl font-bold text-primary uppercase">Request</h1>
+        </h1>
+      </div>
+      <div className="flex flex-row justify-center flex-1 items-center">
+        <p className="text-lg text-bold">
+          I will do my best to play your favorite tune.
+        </p>
+      </div>
+
+      <div className="flex flex-row justify-center flex-1 items-center">
+        <p className="text-lg text-bold">
+          {dj?.artistName}{" "}
+          <span className="text-base">( {dj?.countryCode} )</span>
+        </p>
+      </div>
+
+      <div className="flex flex-row justify-center flex-1 items-center">
+        <Image alt="Dj" width={84} height={30} src="/logo.svg" />
+      </div>
+    </motion.main>
   );
 };
 
