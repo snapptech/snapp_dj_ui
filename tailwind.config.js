228cebf0798b8a79298053e46b519844b784abcb
Varun A P <varunlakshmananap@gmail.com>
2023-07-08 03:29:30 +0200

    add tabs component

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         'primary': '#582DD1',
         'dark': '#1E1E1E',
         'input': '#19192F',
-        'stroke': '#19192F'
+        'stroke': '#19192F',
+        'gray': "#D9D9D9"
       }
     },
   },
