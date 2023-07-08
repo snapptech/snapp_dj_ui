5f42123fd885538523b2989116c9514ec1c98347
Varun A P <varunlakshmananap@gmail.com>
2023-07-08 02:06:47 +0200

    base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'primary': '#582DD1',
+        'dark': '#1E1E1E',
+        'input': '#19192F',
+        'stroke': '#19192F'
+      }
     },
   },
   plugins: [],
