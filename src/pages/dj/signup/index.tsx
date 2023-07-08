5b7e2a882e4db33f4e089478b416b237f9c43613
Swayam Mohanty <smohanty@travix.com>
2023-07-08 04:51:12 +0200

    Alignment fixes signup page absolute to flex

--- src/pages/dj/signup/index.tsx
+++ src/pages/dj/signup/index.tsx
@@ -7,16 +7,19 @@ const handleClick = () => {
 };
 
 const SignUp1 = () => {
-  return <main className=" relative bg-[url(/djsignup.jpg)] bg-cover  h-screen">
-    <h1 className="absolute top-1/2 transform -translate-y-1/2 text-white font-sf-pro text-4xl font-bold leading-13">
+  return <main className="pl-6 pr-6 relative bg-[url(/djsignup.jpg)] bg-cover  h-screen flex flex-col justify-end">
+    <h1 className="pb-6 text-white font-sf-pro text-4xl font-bold leading-13">
       Its how DJ’s make side income
     </h1>
-    <h3 className="text-white font-sf-pro text-lg font-medium leading-7">
+    <h3 className="pb-6  text-white font-sf-pro text-lg font-medium leading-7">
       Let your audience request tracks and tip you during your live sets.
     </h3>
-    <Button type="button" value="Click Me" onClick={handleClick} color="primary" />
-    <br></br>
-    <Button type="button" value="Continue with Google" onClick={handleClick} color="secondary"/>
+    <section className="pb-6 ">
+      <Button type="button" value="Create and Account" onClick={handleClick} color="primary" fullWidth />
+      <div className=" pb-6 " ></div>
+      <Button type="button" value="Continue with Google" onClick={handleClick} color="secondary" fullWidth />
+
+    </section>
 
   </main>;
 };
