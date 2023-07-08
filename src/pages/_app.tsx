ac0dba1175998ebe4f62a519de8605ebe993985a
Varun A P <varunlakshmananap@gmail.com>
2023-07-08 14:04:56 +0200

    generated landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
-    <div className={`min-h-screen ${inter.className}`}>
+    <div
+      className={`min-h-screen ${inter.className} ${
+        router.asPath !== "/" && "mobile"
+      }`}
+    >
       <Component {...pageProps} />
     </div>
   );
